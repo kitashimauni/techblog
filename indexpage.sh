eca0bc08ed01113b006ce8f2eb15035b897d887a
kitashimauni <110659558+kitashimauni@users.noreply.github.com>
2025-07-25 14:44:16 +0900

    fix: output path

--- indexpage.sh
+++ indexpage.sh
@@ -4,6 +4,6 @@ echo "Generating index page with Pagefind..."
 
 wget https://github.com/Pagefind/pagefind/releases/download/v1.3.0/pagefind_extended-v1.3.0-x86_64-unknown-linux-musl.tar.gz
 tar -zxvf pagefind_extended-v1.3.0-x86_64-unknown-linux-musl.tar.gz
-./pagefind_extended --site public --glob="{posts}/*/*.html" --output-path="static/pagefind"
+./pagefind_extended --site public --glob="{posts}/*/*.html"
 
 echo "Index page generated successfully."
